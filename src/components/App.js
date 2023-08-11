74f13b0ffb4a8bf9a4db6853469413907b75a7d1
= <=>
2023-08-11 20:03:51 +0530

    update v1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ParentComponent />
     </div>
   )
 }
 
-export default App
+const ParentComponent = () => {
+
+  let [showModal, setShowModal] = useState(false)
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <ChildComponent showModal={showModal} setShowModal={setShowModal} />
+    </div>
+  )
+}
+
+const ChildComponent = ( {showModal, setShowModal}) => {
+
+  const handleShowModal = () => {
+    setShowModal(true)
+  }
+  return (
+    <div>
+      <h1>Child Component</h1>
+      <button onClick={handleShowModal}>Show Modal</button>
+      {showModal ? (
+        <div>
+          <h3>Modal Content</h3>
+          <p>This is a modal content</p>
+        </div>
+      ): null}
+    </div>
+  )
+}
+
+export default App;
